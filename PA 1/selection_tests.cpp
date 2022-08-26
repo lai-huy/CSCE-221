d6483820964be846ac3628d7b9bb453cf7cba4e5
Huy Lai <lai.huy@tamu.edu>
2022-08-26 12:58:16 -0500

    add test formatting

--- PA 1/selection_tests.cpp
+++ PA 1/selection_tests.cpp
@@ -3,31 +3,121 @@
 
 using std::cout;
 
-int main() {
+#define black   "\033[30m"
+#define red     "\033[31m"
+#define green   "\033[32m"
+#define yellow  "\033[33m"
+#define blue    "\033[34m"
+#define magenta "\033[35m"
+#define cyan    "\033[36m"
+#define white   "\033[37m"
+#define reset   "\033[m"
+
+#define END_TEST bool this_test_passed = test_passed;\
+test_passed = true;\
+return this_test_passed;
+
+#define expect(X) try {\
+  if (!(X)) {\
+    std::cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << "." << reset << "\n";\
+    test_passed = false;\
+  }\
+} catch(...) {\
+  std::cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << #X << " threw an unexpected exception." << reset << "\n";\
+  test_passed = false;\
+}
+
+#define assert(X) try {\
+  if (!(X)) {\
+    std::cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "failed assertion that " << #X << "." << reset << "\n";\
+    test_passed = false;\
+    END_TEST;\
+  }\
+} catch(...) {\
+  std::cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << #X << " assertion threw an unexpected exception." << reset << "\n";\
+  test_passed = false;\
+  END_TEST;\
+}
+
+#define expect_throw(X,E) {\
+  bool threw_expected_exception = false;\
+  try { X; }\
+  catch(const E& err) {\
+    threw_expected_exception = true;\
+  } catch(...) {\
+    std::cout << blue << "  [help] " << #X << " threw an incorrect exception." << reset << "\n";\
+  }\
+  if (!threw_expected_exception) {\
+    std::cout << red <<"  [fail]" << reset << " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << " to throw " << #E <<"." << reset <<"\n";\
+    test_passed = false;\
+  }\
+}
+
+#define expect_no_throw(X) {\
+  try { X; }\
+  catch(...) {\
+    std::cout << red << "  [fail]" << red << " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << " not to throw an excpetion." << reset << "\n";\
+    test_passed = false;\
+  }\
+}
+
+#define test(x) if (test_##x()) { std::cout << green << "[PASS] "; pass_cnt++; }\
+else { std::cout << red << "[FAIL] "; fail_cnt++; }\
+std::cout << #x << reset << "\n";
+#define skip(x) std::cout << yellow << "[SKIP] " << #x << reset << "\n"; skip_cnt++;
+
+#define to_be ==
+#define not_to_be !=
+#define is to_be
+#define is_not not_to_be
+
+namespace {
+    bool test_passed = true;
+}
+
+bool test_select() {
     int* A = new int[7] {8, 6, 7, 5, 3, 0, 9};
-
-    for (int i = 0; i < 9; ++i) {
-        try {
-            select(i, A, 7);
-        } catch (const std::invalid_argument& err) {
-            cout << "Error occured\n";
-        }
+    for (int i = 1; i < 8; ++i) {
+        expect_no_throw(select(i, A, 7));
     }
-
-    try {
-        select(0, A, 0);
-    } catch (const std::invalid_argument& err) {
-        cout << "Error occured\n";
-    }
-
     delete[] A;
-    A = nullptr;
 
-    try {
-        select(0, A, 0);
-    } catch (const std::invalid_argument& err) {
-        cout << "Error occured\n";
-    }
+    END_TEST;
+}
+
+bool test_bounds() {
+    int* A = new int[7] {8, 6, 7, 5, 3, 0, 9};
+    expect_throw(select(0, A, 7), std::invalid_argument);
+    expect_throw(select(8, A, 7), std::invalid_argument);
+    delete[] A;
+
+    END_TEST;
+}
+
+bool test_empty() {
+    expect_throw(select(1, nullptr, 0), std::invalid_argument);
+
+    int* A = new int[0];
+    expect_throw(select(1, A, 0), std::invalid_argument);
+    delete[] A;
+
+    END_TEST;
+}
+
+int main() {
+    unsigned pass_cnt = 0, fail_cnt = 0, skip_cnt = 0;
+
+    test(select);
+    test(bounds);
+    test(empty);
+
+    cout << "\n";
+    cout << magenta << "summary:" << reset << "\n";
+    cout << green << pass_cnt << " tests passing." << reset << "\n";
+    cout << red << fail_cnt << " tests failing." << reset << "\n";
+    cout << yellow << skip_cnt << " tests skipped." << reset << "\n";
+
+    return 0;
     
     return 0;
 }
