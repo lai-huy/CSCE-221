6779ee894d9590a334222a094751fe0ebcab93e5
Huy Lai <lai.huy@tamu.edu>
2022-11-11 23:21:24 -0600

    documentation

--- PA 7/heap.h
+++ PA 7/heap.h
@@ -14,8 +14,18 @@ using std::invalid_argument;
 using std::make_heap, std::push_heap, std::pop_heap;
 using std::reverse;
 
+/**
+ * @brief Pushes the value at the specicified index down the heap according to the specified comparator
+ *
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparator
+ * @param container pointer to the container to edit
+ * @param heap_size size of the heap
+ * @param parent index to push down
+ * @param compare comparator function
+ */
 template <class Container, class Compare = less<typename Container::value_type>>
-void build(Container* container, const size_t& heap_size, const size_t& parent, Compare compare = less<typename Container::value_type>{}) {
+void pushDown(Container* container, const size_t& heap_size, const size_t& parent, Compare compare = less<typename Container::value_type>{}) {
     size_t left = parent << 1, right = left + 1, index = parent;
 
     if (left < heap_size && compare(container->at(left), container->at(parent)))
@@ -24,34 +34,34 @@ void build(Container* container, const size_t& heap_size, const size_t& parent, 
         index = right;
     if (index != parent) {
         swap(container->at(parent), container->at(index));
-        build(container, heap_size, index, compare);
+        pushDown(container, heap_size, index, compare);
     }
 }
 
 /**
- * @brief
+ * @brief converts a container into a heap according to the comparitor
  *
- * @tparam Container
- * @tparam less<typename Container::value_type>
- * @param container
- * @param compare
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparitor class
+ * @param container a pointer to the container to heapify
+ * @param compare comparator to use to construct the heap
  */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heapify(Container* container, Compare compare = less<typename Container::value_type>{}) {
     container->insert(container->begin(), typename Container::value_type());
     size_t heap_size = container->size() - 1;
     for (size_t i = (heap_size >> 1); i >= 1; --i)
-        build(container, heap_size, i, compare);
+        pushDown(container, heap_size, i, compare);
 }
 
 /**
- * @brief
+ * @brief inserts a value into a container according to a comparitor
  *
- * @tparam Container
- * @tparam less<typename Container::value_type>
- * @param container
- * @param value
- * @param compare
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparitor class
+ * @param container a pointer to the container to insert into
+ * @param value value to insert
+ * @param compare comparator
  */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heap_insert(Container* container, const typename Container::value_type& value, Compare compare = less<typename Container::value_type>{}) {
@@ -67,11 +77,11 @@ void heap_insert(Container* container, const typename Container::value_type& val
 }
 
 /**
- * @brief
+ * @brief retreives the first value in the heap
  *
- * @tparam Container
- * @param container
- * @return const Container::value_type&
+ * @tparam Container container class
+ * @param container container to get the first value from
+ * @return const Container::value_type& first value in the heap
  */
 template <class Container>
 typename Container::const_reference heap_get_min(const Container& container) {
@@ -81,12 +91,12 @@ typename Container::const_reference heap_get_min(const Container& container) {
 }
 
 /**
- * @brief
+ * @brief deletes the minimum value from the heap according to a comparator
  *
- * @tparam Container
- * @tparam less<typename Container::value_type>
- * @param container
- * @param compare
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparator class
+ * @param container container to remove from
+ * @param compare comparator to determine the min
  */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heap_delete_min(Container* container, Compare compare = less<typename Container::value_type>{}) {
@@ -95,16 +105,42 @@ void heap_delete_min(Container* container, Compare compare = less<typename Conta
         throw invalid_argument("Heap is empty");
     swap(container->at(1), container->back());
     container->pop_back();
-    build(container, size - 1, 1, compare);
+    pushDown(container, size - 1, 1, compare);
 }
 
 
 // ----------------------- Optional ----------------------- //
+/**
+ * @brief heapity a container using a comparator function
+ *
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparator function
+ * @param container reference to the container
+ * @param compare comparator function
+ */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heapify(Container& container, Compare compare = less<typename Container::value_type>{}) { heapify(&container, compare); }
 
+/**
+ * @brief Insert a value into the heap
+ *
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparator class
+ * @param container reference to the container
+ * @param value value to insert
+ * @param compare comparator function
+ */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heap_insert(Container& container, const typename Container::value_type& value, Compare compare = less<typename Container::value_type>{}) { heap_insert(&container, value, compare); }
 
+
+/**
+ * @brief Delete the minimum value from the heap according to the comparator
+ *
+ * @tparam Container container class
+ * @tparam less<typename Container::value_type> comparator function
+ * @param container container to remove from
+ * @param compare comparator function
+ */
 template <class Container, class Compare = less<typename Container::value_type>>
 void heap_delete_min(Container& container, Compare compare = less<typename Container::value_type>{}) { heap_delete_min(&container, compare); }
--- PA 7/priority_queue.h
+++ PA 7/priority_queue.h
@@ -13,27 +13,102 @@ using std::ostream, std::cout;
 using std::vector, std::less;
 using std::swap, std::move;
 
+/**
+ * @brief Priority Queue
+ *
+ * @tparam Comparable data type the overloads the compairison operators
+ * @tparam Container container type that allows random access
+ * @tparam less<typename Container::value_type> comparator function, std::less by default
+ */
 template <class Comparable, class Container = vector<Comparable>, class Compare = less<typename Container::value_type>>
 class PriorityQueue {
+    /**
+     * @brief Comparator function
+     */
     Compare _compare;
+
+    /**
+     * @brief Container of comparables
+     */
     Container _container;
+
+    /**
+     * @brief the number of elements in the priority queue
+     */
     size_t _size;
 
 public:
+    /**
+     * @brief Construct a new Priority Queue object
+     */
     PriorityQueue() : PriorityQueue(Compare(), Container()) {}
+
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param compare comparator function
+     */
     explicit PriorityQueue(const Compare& compare) : PriorityQueue(compare, Container()) {}
+
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param container container of values
+     */
     explicit PriorityQueue(const Container& container) : PriorityQueue(Compare(), container) {}
+
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param compare comparator function
+     * @param container container of values
+     */
     PriorityQueue(const Compare& compare, const Container& container) : _compare{compare}, _container{container}, _size{container.size()} { heapify(this->_container, this->_compare); }
 
+    /**
+     * @brief Retreives the first element of the priority queue
+     *
+     * @return Container::const_reference the first element of the priority queue
+     */
     typename Container::const_reference top() const { return heap_get_min(this->_container); }
 
+    /**
+     * @brief Determine if the priority queue is empty
+     *
+     * @return true if this is empty
+     * @return false otherwise
+     */
     bool empty() const { return !this->_size; }
+
+    /**
+     * @brief Determine the size of the priority queue
+     *
+     * @return size_t the number of elements in the priority queue
+     */
     size_t size() const { return this->_size; }
 
+    /**
+     * @brief Removes all elements from the priority queue
+     */
     void make_empty() { this->_container.erase(this->_container.begin() + 1, this->_container.end()); this->_size = 0; }
+
+    /**
+     * @brief Pushes a value to the priority queue
+     *
+     * @param val value to add
+     */
     void push(const typename Container::value_type& val) { heap_insert(this->_container, val, this->_compare); ++this->_size; }
+
+    /**
+     * @brief Removes the top value from the priority queue
+     */
     void pop() { heap_delete_min(this->_container, this->_compare); --this->_size; }
 
+    /**
+     * @brief Print the priority queue
+     *
+     * @param os ostream to print to, std::cout by default
+     */
     void print_queue(ostream& os = cout) const {
         if (this->_size) {
             for (typename Container::const_iterator iter = this->_container.begin() + 1; iter != this->_container.end(); ++iter) {
@@ -46,8 +121,24 @@ public:
     }
 
     // ----------------------- Optional ----------------------- //
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param rhs Priority queue to copy from
+     */
     PriorityQueue(const PriorityQueue& rhs) : PriorityQueue(rhs._compare, rhs._container) {}
+
+    /**
+     * @brief Destroy the Priority Queue object
+     */
     ~PriorityQueue() { this->make_empty(); }
+
+    /**
+     * @brief copy assignment operator
+     *
+     * @param rhs priority queue to copy from
+     * @return PriorityQueue& *this
+     */
     PriorityQueue& operator=(const PriorityQueue& rhs) {
         if (this != &rhs) {
             this->make_empty();
@@ -59,26 +150,48 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param compare comparator function
+     * @param container container of comparables
+     */
     PriorityQueue(const Compare& compare, Container&& container) : _compare{compare}, _container{Container()}, _size{container.size()} {
-        swap(this->_container, container);
+        std::swap_ranges(container.begin(), container.end(), this->_container.begin());
         heapify(this->_container, this->_compare);
     }
 
+    /**
+     * @brief Construct a new Priority Queue object
+     *
+     * @param rhs Priority queue to move from
+     */
     PriorityQueue(PriorityQueue&& rhs) : _compare{move(rhs._compare)}, _container{move(rhs._container)}, _size{move(rhs._size)} {
         heapify(this->_container, this->_compare);
     }
 
+    /**
+     * @brief Move assignment operator
+     *
+     * @param rhs Priority queue to move from
+     * @return PriorityQueue& *this
+     */
     PriorityQueue& operator=(PriorityQueue&& rhs) {
         if (this != &rhs) {
             this->make_empty();
             this->_compare = rhs._compare;
-            swap(this->_container, rhs._container);
+            std::swap_ranges(rhs._container.begin(), rhs._container.end(), this->_container.begin());
             swap(this->_size, rhs._size);
         }
 
         return *this;
     }
 
+    /**
+     * @brief Move push a value to the priority queue
+     *
+     * @param val value to move from
+     */
     void push(typename Container::value_type&& val) {
         typename Container::value_type v{};
         swap(v, val);
