65507192137d3e162f13e5a5300cf6eb1f6b79c4
Huy Lai <lai.huy@tamu.edu>
2022-11-15 16:04:26 -0600

    synch

--- PA 9/graph.h
+++ PA 9/graph.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cassert>
 #include <cmath>
 #include <cstddef>
 #include <iostream>
@@ -13,30 +12,140 @@
 #include <vector>
 
 using std::cout, std::ostream;
+using std::vector, std::priority_queue;
+using std::swap, std::move;
+using std::isinf, std::isnan;
+using std::less;
 
 class Graph {
-    Graph();
-    Graph(const Graph& rhs);
-    Graph& operator=(const Graph& rhs);
-    ~Graph();
+private:
+    size_t _size;
+    size_t _edge;
+    vector<vector<double>> _graph;
 
-    size_t vertex_count() const;
-    size_t edge_count() const;
+public:
+    Graph() : _size{0}, _edge{0}, _graph{vector<vector<double>>{}} {}
+    Graph(const Graph& rhs) : _size{rhs._size}, _edge{rhs._edge}, _graph{rhs._graph} {}
+    Graph& operator=(const Graph& rhs) {
+        if (this != &rhs) {
+            this->_size = rhs._size;
+            this->_edge = rhs._edge;
+            this->_graph = rhs._graph;
+        }
 
-    bool contains_vertex(size_t id) const;
-    bool contains_edge(size_t src, size_t dest) const;
-    double cost(size_t src, size_t dest) const;
+        return *this;
+    }
 
-    bool add_vertex(size_t id);
-    bool add_edge(size_t src, size_t dest, double weight = 1.0);
-    bool remove_vertex(size_t id);
-    bool remove_edge(size_t src, size_t dest);
+    ~Graph() { this->_graph.clear(); }
+
+    size_t vertex_count() const { return this->_size; }
+    size_t edge_count() const { return this->_edge; }
+
+    bool contains_vertex(size_t id) const { return id < this->_size; }
+    bool contains_edge(size_t src, size_t dest) const {
+        if (!this->contains_vertex(src))
+            return false;
+        else if (!this->contains_vertex(dest))
+            return false;
+        return !isinf(this->_graph[src][dest]);
+    }
+
+    double cost(size_t src, size_t dest) const { return this->contains_edge(src, dest) ? this->_graph[src][dest] : INFINITY; }
+
+    bool add_vertex(size_t id) {
+        if (id < this->_size)
+            return false;
+        this->_size = id + 1;
+        for (vector<double>& row : this->_graph)
+            row.resize(this->_size, INFINITY);
+        this->_graph.resize(this->_size, vector<double>(this->_size, INFINITY));
+        return true;
+    }
+
+    bool add_edge(size_t src, size_t dest, double weight = 1.0) {
+        if (this->contains_edge(src, dest))
+            return false;
+        if (src == dest)
+            return false;
+        if (!this->contains_vertex(src))
+            return false;
+        if (!this->contains_vertex(dest))
+            return false;
+        if (isinf(weight))
+            return false;
+        if (isnan(weight))
+            return false;
+        this->_graph[src][dest] = weight;
+        ++this->_edge;
+        return true;
+    }
+
+    bool remove_vertex(size_t id) {
+        if (!this->contains_vertex(id))
+            return false;
+
+        for (size_t i = 0; i < this->_size; ++i) {
+            this->remove_edge(i, id);
+            this->remove_edge(id, i);
+        }
+
+        --this->_size;
+        return true;
+    }
+
+    bool remove_edge(size_t src, size_t dest) {
+        if (!this->contains_edge(src, dest))
+            return false;
+        this->_graph[src][dest] = INFINITY;
+        --this->_edge;
+        return true;
+    }
+
+    void dijkstra(size_t source_id) {
+        if (!this->contains_vertex(source_id))
+            return;
+        priority_queue<double, vector<double>, less<double>> pq(less<double>{}, this->_graph[source_id]);
+    }
+
+    double distance(size_t id) const {
+        cout << id;
+        return INFINITY;
+    }
+
+    void print_shortest_path(size_t dest_id, ostream& os = cout) const {
+        if (this->contains_vertex(dest_id)) {
+            return;
+        }
+        os << "<no path>\n";
+    }
+
+    void print_graph(ostream& os = cout) const {
+        if (this->_size) {
+            for (const vector<double>& row : this->_graph) {
+                os << "[";
+                for (size_t j = 0; j < row.size(); ++j) {
+                    os << row[j];
+                    if (j != row.size() - 1)
+                        os << ", ";
+                }
+                os << "]\n";
+            }
+        } else
+            os << "<empty>\n";
+    }
 
     // ----------------------- Optional ----------------------- //
-    // Graph(Graph&& rhs);
-    // Graph& operator=(Graph&& rhs);
+    Graph(Graph&& rhs) : _size{move(rhs._size)}, _edge{move(rhs._edge)}, _graph{move(rhs._graph)} {}
+    Graph& operator=(Graph&& rhs) {
+        if (this != &rhs) {
+            this->_size = 0;
+            this->_edge = 0;
+            this->_graph.clear();
+            this->_size = move(rhs._size);
+            this->_edge = move(rhs._edge);
+            this->_graph = move(rhs._graph);
+        }
 
-    void dijkstra(size_t source_id);
-    double distance(size_t id) const;
-    void print_shortest_path(size_t dest_id, ostream& os = cout) const;
+        return *this;
+    }
 };
--- PA 9/graph_tests.cpp
+++ PA 9/graph_tests.cpp
@@ -1,7 +1,237 @@
 #include "graph.h"
 
+#define black   "\033[30m"
+#define red     "\033[31m"
+#define green   "\033[32m"
+#define yellow  "\033[33m"
+#define blue    "\033[34m"
+#define magenta "\033[35m"
+#define cyan    "\033[36m"
+#define white   "\033[37m"
+#define reset   "\033[m"
+
+#define END_TEST bool this_test_passed = test_passed;\
+test_passed = true;\
+return this_test_passed;
+
+#define expect(X) try {\
+	if (!(X)) {\
+		cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << "." << reset << "\n";\
+		test_passed = false;\
+	}\
+} catch(...) {\
+	cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << #X << " threw an unexpected exception." << reset << "\n";\
+	test_passed = false;\
+}
+
+#define assert(X) try {\
+	if (!(X)) {\
+		cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "failed assertion that " << #X << "." << reset << "\n";\
+		test_passed = false;\
+		END_TEST;\
+	}\
+} catch(...) {\
+	cout << red "  [fail]" reset " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << #X << " assertion threw an unexpected exception." << reset << "\n";\
+	test_passed = false;\
+	END_TEST;\
+}
+
+#define expect_throw(X,E) {\
+	bool threw_expected_exception = false;\
+	try { X; }\
+	catch(const E& err) {\
+		threw_expected_exception = true;\
+	} catch(...) {\
+		cout << blue << "  [help] " << #X << " threw an incorrect exception." << reset << "\n";\
+	}\
+	if (!threw_expected_exception) {\
+		cout << red <<"  [fail]" << reset << " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << " to throw " << #E <<"." << reset <<"\n";\
+		test_passed = false;\
+	}\
+}
+
+#define expect_no_throw(X) {\
+	try { X; }\
+	catch(...) {\
+		cout << red << "  [fail]" << red << " (" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << ") " << red << "expected " << #X << " not to throw an excpetion." << reset << "\n";\
+		test_passed = false;\
+	}\
+}
+
+#define test(x) if (test_##x()) { cout << green << "[PASS] "; pass_cnt++; }\
+else { cout << red << "[FAIL] "; fail_cnt++; }\
+cout << #x << reset << "\n";
+#define skip(x) cout << yellow << "[SKIP] " << #x << reset << "\n"; skip_cnt++;
+
+namespace {
+    bool test_passed = true;
+}
+
+bool test_create() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    END_TEST;
+}
+
+bool test_add_vertex() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(0));
+    assert(g.add_vertex(1));
+    assert(g.add_vertex(2));
+    assert(g.add_vertex(3));
+    assert(g.add_vertex(4));
+    assert(g.add_vertex(5));
+    assert(g.add_vertex(6));
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    END_TEST;
+}
+
+bool test_add_vertex_skip() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    END_TEST;
+}
+
+bool test_add_vertex_fail() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    assert(!g.add_vertex(3));
+
+    END_TEST;
+}
+
+bool test_add_edge() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(0));
+    assert(g.add_vertex(1));
+    assert(g.add_vertex(2));
+    assert(g.add_vertex(3));
+    assert(g.add_vertex(4));
+    assert(g.add_vertex(5));
+    assert(g.add_vertex(6));
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_edge(0, 1, 1.0));
+    assert(g.add_edge(0, 3, 5.0));
+    assert(g.add_edge(1, 3, 3.0));
+    assert(g.add_edge(2, 1, 2.0));
+    assert(g.add_edge(2, 5, 1.5));
+    assert(g.add_edge(4, 2, 0.5));
+    assert(g.add_edge(7, 4, 4.0));
+    assert(g.edge_count() == 7);
+    assert(g.vertex_count() == 8);
+
+    END_TEST;
+}
+
+bool test_add_edge_fail() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(0));
+    assert(g.add_vertex(1));
+    assert(g.add_vertex(2));
+    assert(g.add_vertex(3));
+    assert(g.add_vertex(4));
+    assert(g.add_vertex(5));
+    assert(g.add_vertex(6));
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    assert(!g.add_edge(1, 0, INFINITY));
+    assert(!g.add_edge(0, 1, NAN));
+    assert(!g.add_edge(0, 9, 1.0));
+    assert(!g.add_edge(9, 0, 1.0));
+    assert(!g.add_edge(0, 0, 1.0));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    END_TEST;
+}
+
+bool test_remove_vertex() {
+    Graph g{};
+    assert(g.vertex_count() == 0);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_vertex(0));
+    assert(g.add_vertex(1));
+    assert(g.add_vertex(2));
+    assert(g.add_vertex(3));
+    assert(g.add_vertex(4));
+    assert(g.add_vertex(5));
+    assert(g.add_vertex(6));
+    assert(g.add_vertex(7));
+
+    assert(g.vertex_count() == 8);
+    assert(g.edge_count() == 0);
+
+    assert(g.add_edge(0, 1, 1.0));
+    assert(g.add_edge(0, 3, 5.0));
+    assert(g.add_edge(1, 3, 3.0));
+    assert(g.add_edge(2, 1, 2.0));
+    assert(g.add_edge(2, 5, 1.5));
+    assert(g.add_edge(4, 2, 0.5));
+    assert(g.add_edge(7, 4, 4.0));
+    assert(g.edge_count() == 7);
+    assert(g.vertex_count() == 8);
+
+    assert(g.remove_vertex(2));
+    assert(g.edge_count() == 4);
+    assert(g.vertex_count() == 7);
+
+    END_TEST;
+}
+
 int main() {
-    // TODO(student): write many organized and effective tests
-    
+    unsigned pass_cnt = 0, fail_cnt = 0, skip_cnt = 0;
+
+    test(create);
+    test(add_vertex);
+    test(add_vertex_skip);
+    test(add_vertex_fail);
+    test(add_edge);
+    test(add_edge_fail);
+    test(remove_vertex);
+
+    cout << "\n";
+    cout << magenta << "summary:" << reset << "\n";
+    cout << green << pass_cnt << " tests passing." << reset << "\n";
+    cout << red << fail_cnt << " tests failing." << reset << "\n";
+    cout << yellow << skip_cnt << " tests skipped." << reset << "\n";
+
     return 0;
 }
